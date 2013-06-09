dbace93905a7c54a795f53c9d2bf6b27c9b16a74
stuart bryce <stuart.bryce@sbsitech.com>
2013-06-10 00:46:28 +1000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampeApp::Application.config.secret_key_base = 'da5dcb3aad0ff87042c5ed8aa2a3bf923f2ea1c597854833f5e66876e6df90074e02d642a38c3534ac105c83574662c8f86180ca9e1fc18d05575328511c54e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SampeApp::Application.config.session_store :cookie_store, key: '_Sampe_app_session'
